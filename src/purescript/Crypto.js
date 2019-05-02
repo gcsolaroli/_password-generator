2f638cc1424c9dbd4cb93c133f11c9fcffe92288
Giulio Cesare Solaroli <giuliocesare.solaroli@curaenergy.it>
2019-05-02 13:35:51 +0200

    Added some reference on how to use WebCrypto API

--- src/purescript/Crypto.js
+++ src/purescript/Crypto.js
@@ -2,6 +2,12 @@
 
 // https://github.com/diafygi/webcrypto-examples
 
+/*
+- https://stackoverflow.com/a/22958889
+    If the data is authenticated by an asymmetric signature use CBC, otherwise use GCM.
+
+*/
+
 exports._getRandomValues = function (n) {
     return function () {
         return Buffer.from(crypto.getRandomValues(new Uint8Array(n)));
